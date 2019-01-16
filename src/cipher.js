1990a1808a08f282e811cc18c507e04008d12304
emonroy58 <emgmonroy@gmail.com>
2019-01-16 14:08:17 -0600

    subiendo funcion encode, funcionando

--- src/cipher.js
+++ src/cipher.js
@@ -1,24 +1,22 @@
-let textRecibido ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-window.cipher = {
-   encode: () =>{
-     let result = [];
-    // document.getElementById("txReci").innerHTML = textRecibido;
-    // console.log(textRecibido); 
-  for(let i = 0; i <= textRecibido.length; i++)
-       {
-         console.log(textRecibido.charAt(i));
-         let posicion = textRecibido.charCodeAt(i);//posicion en CodAsc
-         console.log(posicion);
-           let posFin =((posicion-65+33)% 26 +65); //calcular posicion Cipher
-           console.log(posFin);
-         result.push(String.fromCharCode(posFin));//agrega nuevas letras al arreglo
-         console.log(result);
-       }
-         let resulFin = result.join('');
-         document.getElementById('resulCifrar').innerHTML=resulFin;
-         
-    console.log(resulFin);
 
+
+window.cipher = {
+   encode: (offset,textRecibido) =>{
+     let result = [];
+ 
+  for(let i = 0; i < textRecibido.length; i++)
+       {
+         //console.log(textRecibido.charAt(i));
+         let posicion = textRecibido.charCodeAt(i);//posicion en CodAsc
+         //console.log(posicion);
+           let posFin =((posicion-65+offset)% 26 +65); //calcular posicion Cipher
+          // console.log(posFin);
+         result.push(String.fromCharCode(posFin));//agrega nuevas letras al arreglo
+         //console.log(result);
+       }
+         let resultado = result.join('');
+        //console.log(result);
+    return resultado;
     } 
     
 };
