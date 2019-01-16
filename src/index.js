99a24b8371cb6a3cdbea65d824ef1dcf44de2221
emonroy58 <emgmonroy@gmail.com>
2019-01-16 14:09:52 -0600

    subiendo html, estructura funcional y .js llamado de funcion completa

--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
-const botton = document.getElementById('boton');
-botton.addEventListener('click', window.cipher.encode);
+let textRec;
+let off;
+let resul;
+
+
+const botton = document.getElementById('boton'); //ejecuta funcion
+botton.addEventListener('click', ()=>{
+    myfuncion()
+});
+
+function myfuncion(){
+  
+  textRec= document.getElementById('txReci').value;//recibe Texto a Cifrar
+  //console.log(textRec);
+ 
+  off= document.getElementById('offs').value; //Recibe nivel de Offset
+  //console.log(off);
+ 
+ resul = window.cipher.encode(off,textRec)
+ document.getElementById('resulCifrar').innerHTML=resul;
+}
