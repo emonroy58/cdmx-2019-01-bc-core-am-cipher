7f75d3ce7fd5fd0409f0722fa9db5a3f4b899367
emonroy58 <emgmonroy@gmail.com>
2019-01-17 12:30:08 -0600

    Subiendo archivo que contiene funcion encode & decode con test al 100 :)

--- src/cipher.js
+++ src/cipher.js
@@ -1,10 +1,10 @@
 
 
 window.cipher = {
-   encode: (offset,textRecibido) =>{
+   encode: (offset,textRecibido) => {
      let result = [];
  
-  for(let i = 0; i < textRecibido.length; i++)
+  for(let i = 0; i< textRecibido.length; i++)
        {
          //console.log(textRecibido.charAt(i));
          let posicion = textRecibido.charCodeAt(i);//posicion en CodAsc
@@ -17,6 +17,27 @@ window.cipher = {
          let resultado = result.join('');
         //console.log(result);
     return resultado;
-    } 
-    
-};
+  },
+
+
+    decode: (offsetDec,textRecibidoDec) =>{
+      let resultDec = [];
+  
+   for(let i = 0; i < textRecibidoDec.length; i++)
+        {
+          //console.log(textRecibido.charAt(i));
+          let posicionDec = textRecibidoDec.charCodeAt(i);//posicion en CodAsc
+          //console.log(posicion);
+           let posFinDec =((posicionDec+65-offsetDec)% 26 +65); //calcular posicion Cipher
+           // console.log(posFin);
+          resultDec.push(String.fromCharCode(posFinDec));//agrega nuevas letras al arreglo
+          //console.log(result);
+        }
+          let resultadoDec = resultDec.join('');
+         //console.log(result);
+     return resultadoDec;
+     }
+}
+
+
+
