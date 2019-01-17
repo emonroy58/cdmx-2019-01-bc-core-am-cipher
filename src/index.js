40257888f181ae9fd79c22647374aeccef6857af
emonroy58 <emgmonroy@gmail.com>
2019-01-17 12:32:15 -0600

    Subiendo archivo index.html con vista al usuario & index.js con elementos que llaman a la funcion encode y decode

--- src/index.js
+++ src/index.js
@@ -1,44 +1,47 @@
-/*let textRec;
-let off;
-let resul;
+
 
 
 const botton = document.getElementById('boton'); //ejecuta funcion
 botton.addEventListener('click', ()=>{
-    funCifrar()
+    myCifrar()
 });
 
-function funCifrar(){
-  
-  textRec= document.getElementById('txReci').value;//recibe Texto a Cifrar
+
+let textRec;
+let off;
+let resul;
+function myCifrar(){
+
+  textRec= document.getElementById('txReci');//recibe Texto a Cifrar
   //console.log(textRec);
  
-  off= document.getElementById('offs').value; //Recibe nivel de Offset
+  off= document.getElementById('offs'); //Recibe nivel de Offset
   //console.log(off);
  
- resul = window.cipher.encode(off,textRec) //carga los valores de HTML (argumentos) para pasar a la funcion encode yque esta se ejecute
+ resul = window.cipher.encode(off.value,textRec.value) //carga los valores de HTML (argumentos) para pasar a la funcion encode yque esta se ejecute
  document.getElementById('resulCifrar').innerHTML=resul;
-}*/
+}
 
 
 
-let textDecifrado;
-let offDecifra;
-let resulDecifra;
-
-const btnDecifrado = document.getElementById('btnDecifra'); //asignacion de btnDecifra a constante, para ejecutar funcion en la linea posterior
+const btnDecifrado = document.getElementById('decifra'); //asignacion de btnDecifra a constante, para ejecutar funcion en la linea posterior
 btnDecifrado.addEventListener('click', ()=>{
 
      Decifrar()
 });
-
+let textDecifrado;
+let offDecifra;
+let resulDecifra;
 //la funcion recibe los valores de HTML que ingresa el usuario
 function Decifrar(){
-        
-    textDecifrado = document.getElementById('txDecifra');//la variable textDecifrado recibe el texto a decifrar
-    offDecifra = document.getElementById('offsDecifra');//;a variable offDecifra recibe el nivle de desplazamiento(offset)
+    textDecifrado = document.getElementById('txDecifra').value;//la variable textDecifrado recibe el texto a decifrar
+    offDecifra = document.getElementById('offsDecifra').value;//;a variable offDecifra recibe el nivle de desplazamiento(offset)
 
-    resulDecifra = window.cipher.decode(textDecifrado,offDecifra)//carga los valores de HTML (argumentos) para pasar a la funcion decode y que esta se ejecute
+    resulDecifra = window.cipher.decode(offDecifra, textDecifrado);//carga los valores de HTML (argumentos) para pasar a la funcion decode y que esta se ejecute
     document.getElementById('resulDecifrar').innerHTML = resulDecifra;//la variable resulDecifra recibe el resultado de la ejecucion de la funcion decode
+   // console.log(resulDecifra);
+
+
+
 
 }
